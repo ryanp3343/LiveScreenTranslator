fb2928f86e1a15ef2630da8293d2128e4472aa97
Ryan Puertas <ryan_puertas@yahoo.com>
2023-03-24 05:02:24 -0700

    use mss, and queue, and check has_changed function

--- screenrecorder.py
+++ screenrecorder.py
@@ -1,30 +1,49 @@
-import cv2
+import time
+import threading
+from queue import Queue
+from mss import mss
+from PIL import Image, ImageChops
 import pytesseract
-from PIL import ImageGrab
-import numpy as np
-from win32api import GetSystemMetrics
 
-width= GetSystemMetrics(0)
-height = GetSystemMetrics(1)
-# Path to the Tesseract executable file
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+def capture_screenshot():
+    with mss() as sct:
+        monitor = sct.monitors[1]  
+        screenshot = sct.grab(monitor)
+        return Image.frombytes("RGB", screenshot.size, screenshot.rgb)
 
+def ocr_screenshot(image):
+    
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    return pytesseract.image_to_string(image)
 
-while True: 
-    screen = np.array(ImageGrab.grab(bbox=(0,0,width,height)))
-    gray = cv2.cvtColor(screen,cv2.COLOR_BGR2GRAY)
-    threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    clean = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=1)
-    text = pytesseract.image_to_string(clean, lang='eng', config='--psm 11')
-    print(text)
+def has_changed(prev_screenshot, new_screenshot, threshold=5):
+    diff = ImageChops.difference(prev_screenshot, new_screenshot)
+    return diff.getbbox() is not None and diff.getextrema()[0][1] > threshold
 
-    # Display the screenshot
-    cv2.imshow('screen', screen)
+def process_screenshots(queue):
+    while True:
+        screenshot = queue.get()
+        text = ocr_screenshot(screenshot)
+        print("OCR Result:")
+        print(text)
 
-    # Exit if the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def main():
+    capture_interval = 1  
+    
+    screenshot_queue = Queue()
+    processing_thread = threading.Thread(target=process_screenshots, args=(screenshot_queue,))
+    processing_thread.daemon = True
+    processing_thread.start()
 
-# Close the windows
-cv2.destroyAllWindows()
+    prev_screenshot = capture_screenshot()
+
+    while True:
+        time.sleep(capture_interval)
+        new_screenshot = capture_screenshot()
+
+        if has_changed(prev_screenshot, new_screenshot):
+            prev_screenshot = new_screenshot
+            screenshot_queue.put(new_screenshot)
+
+if __name__ == '__main__':
+    main()
