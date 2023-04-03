90573e3a490aeb645ba8aff77d0f00697c142cf5
Ryan Puertas <ryan_puertas@yahoo.com>
2023-04-03 03:16:25 -0700

    added translation function

--- textprocessor.py
+++ textprocessor.py
@@ -1,12 +1,16 @@
 import re
 import nltk
+from googletrans import Translator
 
 class TextProcessor:
     def __init__(self):
-        pass
+        self.translator = Translator()
 
     def process_text(self, text):
         # Remove all non-alphanumeric characters using a regular expression
         cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
-
         return cleaned_text
+    
+    def translate_text(self, text, target_language):
+        translation = self.translator.translate(text, dest=target_language)
+        return translation.text
